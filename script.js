045ae3f0043540804dfa0c1b4e210126855b741a
Miki D <69644623+drmor@users.noreply.github.com.>
2025-09-21 20:30:45 +0500

    Now console can return a random word of 3 (rock, paper, scissors)

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert('Hello World')
+function getComputerChoice(){
+    let num = Math.floor(Math.random() * 3); // 0-2
+    if (num === 0){
+        console.log("rock");
+    } else if (num === 1){
+        console.log("paper");
+    } else if (num === 2){
+        console.log("scissors");
+    }
+};
+
+getComputerChoice();
+
+
+
+
+
+
+
