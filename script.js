a9be45b57fc10c6ee5479231010b0685d8426dc0
Miki D <69644623+drmor@users.noreply.github.com.>
2025-09-22 21:43:02 +0500

    Now you can play one game and see result in console, fixed the return problem

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 let humanScore = 0;
 let computerScore = 0;
 
+function getHumanChoice(){
+    let choice = prompt("What is your choise?", "rock, paper, scissors").toLowerCase();
+    if (choice === "rock"){
+        console.log("Rock");
+    } else if (choice === "paper"){
+        console.log("Paper");
+    } else if (choice === "scissors"){
+        console.log("Scissors");
+    }
+    return choice;
+};
+
 function getComputerChoice(){
     let num = Math.floor(Math.random() * 3); // 0-2
     if (num === 0){
@@ -10,24 +22,54 @@ function getComputerChoice(){
     } else if (num === 2){
         console.log("scissors");
     }
+    return num;
 };
 
-getComputerChoice();
-
-function getHumanChoice(){
-    let choice = prompt("What is your choise?", "rock, paper, scissors");
-    if (choice === "rock"){
-        console.log("Correct")
-    } else if (choice === "paper"){
-        console.log("Correct1")
-    } else if (choice === "scissors"){
-        console.log("Correct2")
+function playRound(humanChoice, computerChoice){
+    if (humanChoice == "rock" && computerChoice == 0){ 
+        humanScore += 1;
+        computerScore += 1;
+        console.log("Tie!");
+    } else if (humanChoice == "rock" && computerChoice == 1){
+        computerScore += 1;
+        console.log("You lose, paper beats rock!");
+    } else if (humanChoice == "rock" && computerChoice == 2){
+        humanScore += 1;
+        console.log("You won, rock beats scissors!");
+    } else if (humanChoice == "paper" && computerChoice == 0){
+        humanScore += 1;
+        console.log("You won, paper beats rock!");
+    } else if (humanChoice == "paper" && computerChoice == 1){
+        humanScore += 1;
+        computerScore += 1;
+        console.log("Tie!");
+    } else if (humanChoice == "paper" && computerChoice == 2){
+        computerScore += 1;
+        console.log("You lose, scissors beats paper!");
+    } else if (humanChoice == "scissors" && computerChoice == 0){
+        computerScore += 1;
+        console.log("You lose, rock beats scissors!");
+    } else if (humanChoice == "scissors" && computerChoice == 1){
+        humanScore += 1;
+        console.log("You won, scissors beats paper!");
+    } else if (humanChoice == "scissors" && computerChoice == 2){
+        humanScore += 1;
+        computerScore += 1;
+        console.log("Tie!");
     }
 }
 
-getHumanChoice();
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
+playRound(humanSelection, computerSelection);
 
+function playGame(){
+    if (humanScore >= 5){
+        console.log("The game ended");
+    } else {
+        playRound(humanSelection, computerSelection);
+    }
+}
 
-
-
+playGame();
