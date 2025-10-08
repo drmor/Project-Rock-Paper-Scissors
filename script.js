503c3f85db7872766197a1cf55ab7524d526f041
Miki D <69644623+drmor@users.noreply.github.com.>
2025-10-09 00:01:06 +0500

    finished the game, fixed bug with game round, now all work fine and clear

--- script.js
+++ script.js
@@ -1,53 +1,64 @@
 const buttons = document.querySelectorAll("button");
-let humanElement;
+let humanScore = 0;
+let computerScore = 0;
+let humanWin = 0;
+let computerWin = 0;
+let currentRound = 0;
+const MAXROUND = 5;
+let humanSelection;
 
+//player element selection and launching the game
 buttons.forEach((btn) => {
     btn.addEventListener("click", () =>{
-        humanElement = btn.id
-        playGame()
+        humanSelection = btn.id
+        playRound(getComputerChoice())
     });
 });
 
-
+//computer element selection
 function getComputerChoice(){
     const elements = ['Rock', 'Paper', 'Scissors'];
     let element = elements[Math.floor(Math.random() * elements.length)];
     return element;
 };
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-    function playRound(humanChoice, computerChoice){  
-        if (humanChoice == computerChoice){ 
-            console.log(`Tie! You and computer chose ${humanChoice}.`);
-        } else if (humanChoice == "Paper" && computerChoice == "Rock" || humanChoice == "Rock" && computerChoice == "Scissors" || humanChoice == "Scissors" && computerChoice == "Paper"){
-            humanScore += 1;
-            console.log(`You won this round, your ${humanChoice} beats computer ${computerChoice}!`);
-        } else {
-            computerScore += 1;
-            console.log(`You lose, computer ${computerChoice} beats your ${humanChoice}!`);
-        } 
-        return [humanScore, computerScore];
+//Rounds operation
+function playRound(computerChoice){
+    if (humanSelection == computerChoice){ 
+        currentRound += 1;
+        console.log(`Tie! You and computer chose ${humanSelection}.`);
+    } else if (humanSelection == "Paper" && computerChoice == "Rock" || humanSelection == "Rock" && computerChoice == "Scissors" || humanSelection == "Scissors" && computerChoice == "Paper"){
+        currentRound += 1;
+        humanScore += 1;
+        console.log(`You won this round, your ${humanSelection} beats computer ${computerChoice}!`);
+    } else {
+        currentRound += 1;
+        computerScore += 1;
+        console.log(`You lose, computer ${computerChoice} beats your ${humanSelection}!`);
+    } 
+    if (currentRound == MAXROUND){
+        gameEnd() 
     }
+    return [humanScore, computerScore];
+}
 
-    let humanSelection = humanElement;
-    let computerSelection = getComputerChoice();
-
-    playRound(humanSelection, computerSelection);
-
-    for(i = 0; i < 4; i++){
-        humanSelection = humanElement;
-        computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-
+//Comparing the score and displaying the result
+function gameEnd(){
     if (humanScore > computerScore){
+        humanWin += 1;
         console.log(`Game ended, you win! With your score ${humanScore}`)
     } else if (humanScore < computerScore){
+        computerWin += 1;
         console.log(`Game ended, you lose! With computer score ${computerScore}`)
     } else if (humanScore == computerScore){
         console.log(`Game ended, with draw! Your and computer score ${humanScore} vs ${computerScore}`)
     }
+    console.log(`Computer: ${computerWin} Player: ${humanWin}`)
+    reset()
 }
-
+//setting all vlues to zero so game can run again and again
+function reset(){
+    humanScore = 0;
+    computerScore = 0;
+    currentRound = 0;
+}
